93c2cdbf3f6b311fd52374340fb9ec76ac346633
Adam Stevens <adam23stevens@gmail.com>
2017-04-24 22:32:48 +0100

    init proj

---
+++ Entities/Story.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities
+{
+    public class Story
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+        public bool IsActive { get; set; }
+        public List<User> CurrentUsers { get; set; }
+        public int MaxUsers { get; set; }
+        public List<StoryPart> StoryParts { get; set; }
+        public bool HasRudewords { get; set; }
+        public List<Rating> Ratings { get; set; }
+        public float AverageScore { get; set; }
+        public bool IsPublished { get; set; }
+    }
+}
